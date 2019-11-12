8719adfe86bb5a931ca27d574e9abbe67aba271f
Mithun Kumar Adhikary <12178@palonet.org>
2019-11-12 19:44:06 +0600

    Composing Components

--- react/counter-app/src/components/counter.jsx
+++ react/counter-app/src/components/counter.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+
+    componentDidUpdate(prevProps, prevState){
+        if(prevProps.counter.value !== this.props.counter.value){
+
+        }
+    }
+
+    componentWillUnmount() {
+
+    }
+
     state = {
-        count:0,
+        value:this.props.counter.value,
         tags: ["tag1","tag2","tag3"]
     };
 
@@ -13,8 +24,7 @@ class Counter extends Component {
     }
 
     handleIncrement = (product) => {
-        console.log(product)
-        this.setState({count: this.state.count + 1})
+        this.setState({value: this.state.value + 1})
     }
 
     doHandleIncrement = () => {
@@ -22,35 +32,49 @@ class Counter extends Component {
     };
 
     render() { 
-       
+        console.log('props',this.props);       
         return (
             
             // <div>
             //     {this.state.tags.length === 0 && "Please create a new tag"}
             //     {this.renderTags()}
             // </div>
-                <div>
-                    <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                    <button 
-                        onClick = {this.doHandleIncrement}
-                        className="btn btn-secondary btn-sm"
-                    >
-                        Increment
-                    </button>
+                <div className="row">
+                    <div className="col-1">
+                        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                    </div>
+                    <div className="col">
+                        <button 
+                            onClick = {() => this.props.onIncrement(this.props.counter)}
+                            className="btn btn-secondary btn-sm"
+                        >
+                            +
+                        </button>
+                        <button 
+                            onClick = {() => this.props.onDecrement(this.props.counter)}
+                            className="btn btn-secondary btn-sm m-2"
+                            disabled={this.props.counter.value === 0 ? "disabled":""}
+                        >
+                            -
+                        </button>
+                        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm">Delete</button>
+                    </div>
+                    
+                    
                 </div>
             );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.props.counter.value === 0) ? "warning" : "primary";
 
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count
+        const { value } = this.props.counter;
+        return value === 0 ? 'Zero' : value
     }
 }
  
--- react/vidly/src/components/movies.jsx
+++ react/vidly/src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import Like from "./common/like";
 
 class Movies extends Component {
     state = { 
@@ -10,6 +11,14 @@ class Movies extends Component {
         const movies = this.state.movies.filter(m => m._id !== movie._id);
 
         this.setState({movies})
+    }
+
+    handleLike = (movie) => {
+       const movies = [...this.state.movies];
+       const index = movies.indexOf(movie);
+       movies[index] = {...movies[index]};
+       movies[index].liked = !movies[index].liked;
+       this.setState({movies});
     }
 
     render() { 
@@ -28,6 +37,7 @@ class Movies extends Component {
                                 <th>Stock</th>
                                 <th>Rate</th>
                                 <th></th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -37,6 +47,9 @@ class Movies extends Component {
                                     <td>{movie.genre.name}</td>
                                     <td>{movie.numberInStock}</td>
                                     <td>{movie.dailyRentalRate}</td>
+                                    <td>
+                                        <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
+                                    </td>
                                     <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                                 </tr>
                             ))}
